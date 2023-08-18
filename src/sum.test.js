b99f4b3071023728cf498edd2def588d7527d1f4
DESKTOP-DKTVU9O\Sabina <hisamovasabina5@gmail.com>
2023-08-18 23:44:48 +0500

    add eslint

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,8 +1,7 @@
-import {sum} from "./sum";
+import { sum } from './sum';
 
-describe("sum", () =>{
-    it("sum number", () => {
-        expect(sum(1,2)).toEqual(3);
-    })
-})
-
+describe('sum', () => {
+  it('sum number', () => {
+    expect(sum(1, 2)).toEqual(3);
+  });
+});
