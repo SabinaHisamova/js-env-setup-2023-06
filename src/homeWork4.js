8b4337e6d7a9394788a7088df67ba7439dc4c93e
DESKTOP-DKTVU9O\Sabina <hisamovasabina5@gmail.com>
2023-09-04 21:19:38 +0500

    add homework5 and tests fot it

--- src/homeWork4.js
+++ src/homeWork4.js
@@ -2,7 +2,7 @@ export function objAge() {
     let user = {
       name: "John"
     };
-    const age1 = prompt("Enter a number");
+    const age1 = prompt("Enter age for obj user");
     user.age = +age1;
 
     let admin = Object.assign({}, user, {role: 'admin'});
